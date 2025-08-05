c37b32f9bd015a40fb063316e7bdb3467d1ba488
Noah Amoo <yawnoah3n@yahoo.com>
2025-08-05 23:29:53 +0100

    Created the function to add new todos

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,24 @@
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+
 import { data } from '@/data/todos'
 
 export default function Index() {
+  const [todos, setTodos] = useState(data.sort((a,b) => b.id - a.id))
+  const [text, setText] = useState('')
+
+  const addTodo = () => {
+    if (text.trim()) {
+      const newId = todos.length > 0 ? todos[0].id + 1 : 1;
+      setTodos([{ id: newId, title: text, completed: false, ...todos}])
+      setText('')
+    } 
+  }
+
   return (
     <SafeAreaView>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <View>
         <Text>Edit app/index.tsx to edit this screen.</Text>
       </View>
     </SafeAreaView>
