f49588d3440d53af012048c43d25166b8292c2da
Noah Amoo <yawnoah3n@yahoo.com>
2025-08-09 09:10:32 +0100

    Defined light and dark colors in Colors in the constants directory

---
+++ constants/Colors.ts
@@ -0,0 +1,14 @@
+export const Colors = {
+    light: {
+        text: 'black',
+        backkground: 'white',
+        icon: 'black',
+        button: 'royalblue',
+    },
+    dark: {
+        text: 'white',
+        backkground: 'black',
+        icon: 'red',
+        button: 'white',
+    },
+};
