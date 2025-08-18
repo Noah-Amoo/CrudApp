d4162c4db7e1c4f093ce92ea24da358ccc88f44f
Noah Amoo <yawnoah3n@yahoo.com>
2025-08-18 17:52:04 +0100

    Created the fetch data function in a useeffect hook to load the details of a todo based on its id

--- app/todos/[id].jsx
+++ app/todos/[id].jsx
@@ -1,8 +1,47 @@
 import { useLocalSearchParams } from "expo-router";
 import { View, Text, StyleSheet, Pressable, TextInput } from 'react-native'
 
+import { useState, useEffect, useContext } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemeContext } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
+import Octicons from "@expo/vector-icons/Octicons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
+
 export default function EditScreen() {
     const { id } = useLocalSearchParams()
+    const [todo, setTodo] = useState({})
+    const { colorScheme, setColorScheme, theme } = useContext(ThemeContext)
+    const router = useRouter()
+
+    const [loaded, error] = useFonts({
+    Inter_500Medium,
+    })
+
+    // This hook calls the fetch data to load the details of a todo based on its id
+    useEffect(() => {
+        const fetchData = async (id) => {
+            try {
+                const jsonValue = await AsyncStorage.getItem("TodoApp")
+                const storageTodos = jsonValue != null ? JSON.parse(jsonValue) : null;
+
+                if (storageTodos && storageTodos.length) {
+                    const myTodo = storageTodos.find(todo => todo.id.toSTring() === id)
+                    setTodo(myTodo)
+                }
+            } catch (e) {
+                console.error(e)
+            }
+        }
+
+        fetchData(id)
+    }, [id])
+
+    if (!loaded && !error) {
+        return null
+    }
 
     return (
         <View>
